ca631b1fde7d0570d452cc303c5eaa36ab190633
Zettios <77236807+Zettios@users.noreply.github.com>
2023-12-13 16:30:24 +0100

    Added start for the splash screen & the top and bottom nav bars.

---
+++ app/src/main/java/com/example/auto_maatklantenapp/TopNavigationFragment.java
@@ -0,0 +1,9 @@
+package com.example.auto_maatklantenapp;
+
+import androidx.fragment.app.Fragment;
+
+public class TopNavigationFragment extends Fragment {
+    public TopNavigationFragment() {
+        super(R.layout.fragment_top_navigation);
+    }
+}
