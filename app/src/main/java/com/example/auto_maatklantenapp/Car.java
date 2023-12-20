87f7b88546113faaaee73c7fade2172b78d2d181
Zettios <77236807+Zettios@users.noreply.github.com>
2023-12-20 12:58:15 +0100

    Start car list. Switched from weights to DPs regarding main layout.

---
+++ app/src/main/java/com/example/auto_maatklantenapp/Car.java
@@ -0,0 +1,18 @@
+package com.example.auto_maatklantenapp;
+
+public class Car {
+
+    String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/example/auto_maatklantenapp/RecyclerViewHolder.java
@@ -0,0 +1,20 @@
+package com.example.auto_maatklantenapp;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecyclerViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView view;
+    public RecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+        view = itemView.findViewById(R.id.randomText);
+    }
+
+    public TextView getView(){
+        return view;
+    }
+}
