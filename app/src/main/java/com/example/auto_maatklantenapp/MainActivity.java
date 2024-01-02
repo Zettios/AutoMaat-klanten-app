233106279ecb0674bf513e4823644ea32dbb3ac5
AdamHasa <adamhasanin@hotmail.nl>
2024-01-02 17:10:58 +0100

    get data from the backend

--- app/src/main/java/com/example/auto_maatklantenapp/MainActivity.java
+++ app/src/main/java/com/example/auto_maatklantenapp/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.auto_maatklantenapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mTextViewResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewResult = findViewById(R.id.textViewCars);
+
+        OkHttpClient client = new OkHttpClient();
+
+        String url = "https://measured-adder-concrete.ngrok-free.app/api/cars";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()){
+                    String myResponse = response.body().string();
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextViewResult.setText(myResponse);
+                        }
+                    });
+                }
+
+            }
+        });
     }
 }
